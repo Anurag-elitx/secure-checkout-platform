0c283519f54f4077ce0be4feb57aa25a5850c072
Anurag <kumaranurag50534@gmail.com>
2025-12-07 23:54:24 +0530

    Development update - Batch 5

---
+++ backend/api/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+# Integration tests for product API
+
---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # Import the views from views.py
+
+urlpatterns = [
+    path('', views.getRoutes, name="getRoutes"),
+    path('products/', views.getProducts, name="getProducts"),
+]
